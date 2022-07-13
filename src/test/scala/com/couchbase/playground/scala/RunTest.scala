c96ab91512f1da26c45a1b2b6974082741543159
Bas van Gijzel <bas.vangijzel@couchbase.com>
2022-07-13 14:42:53 +0100

    Fix tests

--- src/test/scala/com/couchbase/playground/scala/RunTest.scala
+++ src/test/scala/com/couchbase/playground/scala/RunTest.scala
@@ -6,7 +6,7 @@ class RunTest extends AnyFlatSpec {
   val m = new Main()
 
   "Evaluating simple code" should "return expected result" in {
-    assertResult(Right(42))(m.evalString("13 + 29"))
+    assertResult(Right(CodeResult("", 42)))(m.evalString("13 + 29"))
   }
 
   "Evaluating non-compiling code" should "return error message" in {
@@ -16,10 +16,10 @@ class RunTest extends AnyFlatSpec {
   "Evaluate code requiring Couchbase jars" should "return expected result" in {
     val code = """    import com.couchbase.client.core.error.CouchbaseException
                  |    val ce = new CouchbaseException("Hello")
-                 |    println(ce.getMessage)
+                 |    print(ce.getMessage)
                  |    ce.getMessage
                  |""".stripMargin
 
-    assertResult(Right("Hello"))(m.evalString(code))
+    assertResult(Right(CodeResult("Hello", "Hello")))(m.evalString(code))
   }
 }
